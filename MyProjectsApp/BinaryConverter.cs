3a626d630130c61ba9abd67edd9f8908f26902c4
Charlie <charlescwallace2017@gmail.com>
2024-06-27 17:51:22 -0700

    first commit;

---
+++ MyProjectsApp/BinaryConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class BinaryConverter
+{
+    public BinaryConverter()
+    {
+    }
+
+    public static double GetDecimalFromBinary(string binaryInput)
+    {
+        double totalValue = 0;
+
+        int inputLength = binaryInput.Length;
+
+        for (int i = 0; i < inputLength; i++)
+        {
+            double currentBinaryDigit = Convert.ToDouble(binaryInput[i].ToString());
+
+            if (currentBinaryDigit == 0)
+                continue;
+
+            double actualDigitValue = Math.Pow(2, Math.Abs(7 - i));
+
+            totalValue += actualDigitValue;
+        }
+
+        return totalValue;
+    }
+}
